7a9c83daa62127a497011da5214ea1fbf175e062
yungblud <yungblud@kakao.com>
2020-05-18 18:31:13 +0900

    readFile ?

--- source/api/handleError.ts
+++ source/api/handleError.ts
@@ -1,6 +1,14 @@
 import {
     jsonResponse,
+    textResponse
 } from "https://deno.land/x/reno@v1.0.0-alpha.1/reno/mod.ts";
+
+export const methodNotAllowedResponse = (url: string, method: string) => {
+    return {
+        status: 405,
+        ...textResponse(`Method ${method} not allowed for ${url}`)
+    }
+}
 
 export const createErrorResponse = (statusCode: number, e: any) => {
     return jsonResponse({
--- source/api/todo.ts
+++ source/api/todo.ts
@@ -4,29 +4,34 @@ import {
     // createRouteMap,
     // textResponse,
     jsonResponse,
+    JsonRequest
     // streamResponse,
   } from "https://deno.land/x/reno@v1.0.0-alpha.1/reno/mod.ts";
+//   import { readJson, readJsonSync } from "https://deno.land/std/fs/mod.ts";
+import { methodNotAllowedResponse } from "./handleError.ts"
+import data from "./data.ts"
 
-const data = [
-    {
-        id: 1,
-        title: "Laundry"
-    },
-    {
-        id: 2,
-        title: "Walking"
-    },
-    {
-        id: 3,
-        title: "Working"
-    },
-    {
-        id: 4,
-        title: "Tea Time"
-    }
-]
+// const data = [
+//     {
+//         id: 1,
+//         title: "Laundry"
+//     },
+//     {
+//         id: 2,
+//         title: "Walking"
+//     },
+//     {
+//         id: 3,
+//         title: "Working"
+//     },
+//     {
+//         id: 4,
+//         title: "Tea Time"
+//     }
+// ]
 
 export const todos = () => {
+    const { readFileSync } = Deno
     return jsonResponse([
         {
             id: 1,
@@ -47,10 +52,23 @@ export const todos = () => {
     ])
 }
 
-export const todo = (req: Pick<AugmentedRequest, "routeParams">) => {
+export const todo = (req: JsonRequest) => {
+    const { writeFileSync } = Deno
     const [id] = req.routeParams
-    const filteredTodo = data.filter((todoItem) => todoItem.id === +id)
-    return jsonResponse({
-        todo: filteredTodo.length === 1 ? filteredTodo[0] : null
-    })
+    const { url, method } = req
+    switch (method) {
+        case "GET":
+            const filteredTodo = data.filter((todoItem) => todoItem.id === +id)
+            return jsonResponse({
+                todo: filteredTodo.length === 1 ? filteredTodo[0] : null
+            })
+        case "POST":
+            return jsonResponse({
+                hello: "world"
+            })
+        default:
+            return methodNotAllowedResponse(url, method)
+    }
 }
+
+
